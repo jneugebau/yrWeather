42acfe874bce26ba7837a3ea9e9e81ec0ad878ab
jn <joerg.neugebauer@me.com>
2019-05-19 11:21:36 +0200

    Initial Commit

---
+++ yrWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  yrWeather
+//
+//  Created by Joerg Neugebauer on 19.05.19.
+//  Copyright © 2019 Joerg Neugebauer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
