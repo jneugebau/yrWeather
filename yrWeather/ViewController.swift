d7b2a3169f23ad379478acd3c7694c461eb34b32
jn <joerg.neugebauer@me.com>
2019-05-20 20:34:07 +0200

    added laugh screen and app icon

--- yrWeather/ViewController.swift
+++ yrWeather/ViewController.swift
@@ -8,11 +8,41 @@
 
 import UIKit
 
+/*
+extension UIView {
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+}
+*/
+
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var navigationBar: UIView!
+    @IBOutlet weak var toolBar: UIView!
+    @IBOutlet weak var lblCity: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationBar.layer.cornerRadius = 10
+        navigationBar.layer.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.65).cgColor
+        
+        toolBar.layer.cornerRadius = 10
+        toolBar.layer.borderWidth = 1
+        toolBar.layer.borderColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.65).cgColor
+        toolBar.layer.backgroundColor = UIColor.clear.cgColor
+        
+        lblCity.text = "Munich"
+        
     }
 
 
