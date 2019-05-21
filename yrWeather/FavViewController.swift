68006a5d9b60af8b8223f9f220c4d58bdb78a5fb
jn <joerg.neugebauer@me.com>
2019-05-21 15:49:11 +0200

    style modifications to favorites table view

--- yrWeather/FavViewController.swift
+++ yrWeather/FavViewController.swift
@@ -8,17 +8,78 @@
 
 import UIKit
 
-class FavViewController: UIViewController {
+class FavViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var cities:[String] = ["Munich", "New York", "Los Angeles", "Sydney", "Hongkong"]
+    
 
     @IBOutlet weak var btnClose: UIButton!
+    @IBOutlet weak var btnAdd: UIButton!
+    @IBOutlet weak var tblViewFavorites: UITableView!
     
     let clRed:CGColor = UIColor(red: 238.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1.0).cgColor
     let clBlue:CGColor = UIColor(red: 0/255.0, green: 153.0/255.0, blue: 255.0/255.0, alpha: 1.0).cgColor
     let clOrange:CGColor = UIColor(red: 255.0/255.0, green: 204.0/255.0, blue: 102.0/255.0, alpha: 1.0).cgColor
     
     
+    /*
+     * close favorites view
+     */
     @IBAction func buttonCloseClicked(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+     * add new location to favorites list
+     */
+    @IBAction func buttonAddClicked(_ sender: Any) {
+        
+    }
+    
+    /*
+     *
+     */
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor.clear
+        return headerView
+    }
+    
+    /*
+     * delete row from table view
+     */
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete{
+            cities.remove(at: indexPath.row)
+            tblViewFavorites.reloadData()
+        }
+    }
+    
+    /*
+     *
+     */
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 8.0
+    }
+    
+    /*
+     * return number of rows
+     */
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cities.count
+    }
+    
+    /*
+     * fill rows
+     */
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = cities[indexPath.row]
+        cell.layer.masksToBounds = true
+        cell.layer.cornerRadius = 5
+        cell.layer.borderWidth = 1
+        cell.layer.borderColor = clBlue
+        return cell
     }
     
     override func viewDidLoad() {
@@ -26,6 +87,8 @@ class FavViewController: UIViewController {
         
         btnClose.layer.cornerRadius = 10
         btnClose.layer.backgroundColor = clBlue
+
+
 
         // Do any additional setup after loading the view.
     }
